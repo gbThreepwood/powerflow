794005f8fce3cb35334c0913fbe949c3ede7e37e
Eirik Haustveit <eirik.haustveit@gmail.com>
2014-12-08 12:57:01 +0100

    Update opf.py

--- opf.py
+++ opf.py
@@ -1,1 +1,37 @@
- 
+#!/usr/bin/env python
+##
+# vim:sw=8:ts=8:si:et
+# To use the above modeline in vim you must have "set modeline" in your .vimrc
+#
+# Filename: print_result.py
+#
+# Created on: 06. des. 2014
+#
+# Author: Eirik Haustveit
+#
+# Copyright (c) Eirik Haustveit, 2014
+#
+#
+# This file is part of powerflow.
+#
+# powerflow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# powerflow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with libXplained. If not, see <http://www.gnu.org/licenses/>.
+##
+__author__ = "Eirik Haustveit"
+__copyright__ = "Copyright 2014, Eirik Haustveit"
+__credits__ = ["various"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Eirik Haustveit"
+__email__ = "eirik@la2kta.net"
+__status__ = "Development"
